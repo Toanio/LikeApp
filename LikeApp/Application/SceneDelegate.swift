330c21313429123647f2b175c794232727ae0539
Toan <chan.misha95@mail.ru>
2023-05-21 04:53:23 +0300

    - Первоначальная настройка проекта

---
+++ LikeApp/Application/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  LikeApp
+//
+//  Created by c.toan on 21.05.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = MainViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
+
