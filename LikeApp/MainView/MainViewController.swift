dec60c1711f5126e534d8ae9b7c22d92f6f5d89c
Toan <chan.misha95@mail.ru>
2023-05-22 17:28:27 +0300

    - Добавл SnapKit - Добавил кнопку на MainView

--- LikeApp/MainView/MainViewController.swift
+++ LikeApp/MainView/MainViewController.swift
@@ -6,9 +6,25 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
+    var addButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "plus.circle.fill"), for: .normal)
+        button.tintColor = .green
+        button.addTarget(self, action: #selector(addButtonClicked), for: .touchUpInside)
+        return button
+    }()
+    @objc func addButtonClicked() {
+        print("hello")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(addButton)
+        addButton.snp.makeConstraints { make in
+              make.center.equalToSuperview()
+        }
     }
 }
